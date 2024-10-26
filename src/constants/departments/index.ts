4c54d12555aad424800ce8b3ebc9cf9dd8b0a856
wasxyz <wasaki@acee.info>
2024-10-26 22:26:09 +0900

    Update package.json and build files2

--- src/constants/departments/index.ts
+++ src/constants/departments/index.ts
@@ -4,15 +4,58 @@ export const DEPARTMENTS: DepartmentList = Object.freeze({
   ADMINISTRATION: {
     name: '総務部',
     id: 1,
+    // その他の部門情報
   },
   ACCOUNTING: {
     name: '経理部',
     id: 2,
+    // その他の部門情報
   },
-  SALES: {
-    name: '営業部',
+  TOKYO_BRANCH: {
+    name: '東京支店',
     id: 3,
-  }
+    // その他の部門情報
+  },
+  KANTO_BRANCH: {
+    name: '関東支店',
+    id: 4,
+    // その他の部門情報
+  },
+  NAGOYA_BRANCH: {
+    name: '名古屋支店',
+    id: 5,
+    // その他の部門情報
+  },
+  KYOTO_BRANCH: {
+    name: '京都支店',
+    id: 6,
+    // その他の部門情報
+  },
+  KANSAI_BRANCH_OFFICE: {
+    name: '関西支社',
+    id: 7,
+    // その他の部門情報
+  },
+  KOBE_BRANCH: {
+    name: '神戸支店',
+    id: 8,
+    // その他の部門情報
+  },
+  FUKUOKA_BRANCH: {
+    name: '福岡支店',
+    id: 9,
+    // その他の部門情報
+  },
+  NISHINOMIYA_LOGISTICS_CENTER: {
+    name: '西宮物流センター',
+    id: 10,
+    // その他の部門情報
+  },
+  KANSAI_LOGISTICS_CENTER: {
+    name: '関西物流センター',
+    id: 11,
+    // その他の部門情報
+  },
 } as const);
 
 export { Department, DepartmentList };
